05f66f57005720ecc14df09f8b38d3d6cb1b7acd
Pneumana <connor_nicewarner1@my.vcccd.edu>
2024-01-14 16:27:45 -0800

    Card Inspect Script

--- UnityProject/Assets/ConnorTiltCard.cs
+++ UnityProject/Assets/ConnorTiltCard.cs
@@ -6,10 +6,17 @@ public class ConnorTiltCard : MonoBehaviour
 {
     public float constraints;
     public float speed;
+    float x;
+    float z;
+    public Vector3 offset;
+
+    Ray ray;
+    RaycastHit hit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.rotation.eulerAngles;
     }
 
     // Update is called once per frame
@@ -32,16 +39,30 @@ public class ConnorTiltCard : MonoBehaviour
         {
             input.z = -1;
         }
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject == gameObject)
+            {
+                var diff = new Vector3(hit.point.z, 0, hit.point.x) - new Vector3(transform.position.z, 0, transform.position.x);
+                //new Vector3(hit.point.z, 0, hit.point.x) - new Vector3(transform.position.z, 0, transform.position.x);
+                input = new Vector3(1 * diff.x,1,-1 * diff.z);
+                //Debug.Log(diff + "=>" + diff.normalized);
+            }
+        }
         if (input != Vector3.zero)
         {
             var move = Vector3.MoveTowards(transform.localEulerAngles, transform.localEulerAngles + (input * constraints), Time.deltaTime * speed);
-            transform.rotation = Quaternion.Euler(move);
+            x = Mathf.MoveTowards(x, input.x * constraints, Time.deltaTime * speed);
+            z = Mathf.MoveTowards(z, input.z * constraints, Time.deltaTime * speed);
         }
         else
         {
-/*            //
-            var move = Vector3.MoveTowards(transform.localEulerAngles, Vector3.zero, Time.deltaTime * speed);
-            transform.rotation = Quaternion.Euler(move);*/
+            x = Mathf.MoveTowards(x, 0, Time.deltaTime * speed);
+            z = Mathf.MoveTowards(z, 0, Time.deltaTime * speed);
         }
+        transform.rotation = Quaternion.Euler(new Vector3(x + offset.x, 0+offset.y, z + offset.z));
+
+
     }
 }
