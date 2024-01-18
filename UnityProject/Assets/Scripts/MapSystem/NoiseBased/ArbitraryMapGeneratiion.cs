525d34f66b261de2a1063c7628133ab98834be11
Pneumana <connor_nicewarner1@my.vcccd.edu>
2024-01-17 20:47:22 -0800

    mapgen now picks 2 rooms furthest from each other
    also visually represented by tiles

--- UnityProject/Assets/Scripts/MapSystem/NoiseBased/ArbitraryMapGeneratiion.cs
+++ UnityProject/Assets/Scripts/MapSystem/NoiseBased/ArbitraryMapGeneratiion.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class ArbitraryMapGeneratiion : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class ArbitraryMapGeneratiion : MonoBehaviour
     public List<Vector3> placedRooms = new List<Vector3>();
 
     public List<List<Vector3>> adjacantRooms = new List<List<Vector3>>();
+
+    public Sprite icon;
     //need a list of adjacant positions for each placed room, excluding positions that are already used
 
     private void Start()
@@ -35,7 +38,7 @@ public class ArbitraryMapGeneratiion : MonoBehaviour
                 int b = (int)Random.Range(0, gridSize.y - 1);
                 mapPositions.Remove(new Vector2(a,b));
                 placedRooms.Add(new Vector2(a, b));
-                List<Vector3> neighbors = new List<Vector3>();
+                /*List<Vector3> neighbors = new List<Vector3>();
                 if(!placedRooms.Contains(new Vector2(a + 1, b)))
                     neighbors.Add(new Vector2(a + 1, b));
                 if (!placedRooms.Contains(new Vector2(a - 1, b)))
@@ -43,9 +46,9 @@ public class ArbitraryMapGeneratiion : MonoBehaviour
                 if (!placedRooms.Contains(new Vector2(a, b + 1)))
                     neighbors.Add(new Vector2(a, b + 1));
                 if (!placedRooms.Contains(new Vector2(a, b - 1)))
-                    neighbors.Add(new Vector2(a, b - 1));
+                    neighbors.Add(new Vector2(a, b - 1));*/
 
-                adjacantRooms.Add(neighbors);
+                adjacantRooms.Add(CheckValidPosition(a,b));
             }
             else
             {
@@ -64,29 +67,87 @@ public class ArbitraryMapGeneratiion : MonoBehaviour
                 List<Vector3> neighbors = new List<Vector3>();
                 var a = pick.x;
                 var b = pick.y;
-                if (!placedRooms.Contains(new Vector2(a + 1, b)))
-                    neighbors.Add(new Vector2(a + 1, b));
-                if (!placedRooms.Contains(new Vector2(a - 1, b)))
-                    neighbors.Add(new Vector2(a - 1, b));
-                if (!placedRooms.Contains(new Vector2(a, b + 1)))
-                    neighbors.Add(new Vector2(a, b + 1));
-                if (!placedRooms.Contains(new Vector2(a, b - 1)))
-                    neighbors.Add(new Vector2(a, b - 1));
 
-                adjacantRooms.Add(neighbors);
+                adjacantRooms.Add(CheckValidPosition((int)a,(int)b));
                 //find a random neighbor room
                 //add to placed rooms
                 //loop trhrough all neighbor lists and remove that position
             }
             placeAttempts--;
         }
+        Vector3 furthestPair1 = Vector3.zero;
+        Vector3 furthestPair2 = Vector3.zero;
+        float dist = 0;
         foreach(Vector3 pos in placedRooms)
         {
-            Debug.DrawLine(pos + new Vector3(0.5f, -0.5f), pos + new Vector3(-0.5f, 0.5f), Color.green, 10);
-            Debug.DrawLine(pos + new Vector3(-0.5f, -0.5f), pos + new Vector3(0.5f, 0.5f), Color.green, 10);
+            /*Debug.DrawLine(pos + new Vector3(0.5f, -0.5f), pos + new Vector3(-0.5f, 0.5f), Color.green, 10);
+            Debug.DrawLine(pos + new Vector3(-0.5f, -0.5f), pos + new Vector3(0.5f, 0.5f), Color.green, 10);*/
+            var tile = new GameObject();
+            tile.name = "MapTile";
+            tile.transform.position = pos;
+            tile.AddComponent<SpriteRenderer>();
+            tile.GetComponent<SpriteRenderer>().sprite = icon;
+            foreach(Vector3 ps in placedRooms)
+            {
+                if (ps != pos)
+                {
+                    var measure = Vector3.Distance(pos, ps);
+                    if (measure > dist)
+                    {
+                        dist = measure;
+                        furthestPair1 = ps;
+                        furthestPair2 = pos;
+                    }
+                }
+            }
         }
+        Debug.DrawLine(furthestPair1 + new Vector3(0.5f, -0.5f), furthestPair1 + new Vector3(-0.5f, 0.5f), Color.green, 10);
+        Debug.DrawLine(furthestPair1 + new Vector3(-0.5f, -0.5f), furthestPair1 + new Vector3(0.5f, 0.5f), Color.green, 10);
+
+        Debug.DrawLine(furthestPair2 + new Vector3(0.5f, -0.5f), furthestPair2 + new Vector3(-0.5f, 0.5f), Color.red, 10);
+        Debug.DrawLine(furthestPair2 + new Vector3(-0.5f, -0.5f), furthestPair2 + new Vector3(0.5f, 0.5f), Color.red, 10);
+
+        Debug.DrawLine(gridSize * new Vector3(0,0), gridSize * new Vector3(1, 0), Color.cyan, 30);
+        Debug.DrawLine(gridSize * new Vector3(1, 0), gridSize * new Vector3(1, 1), Color.cyan, 30);
+        Debug.DrawLine(gridSize * new Vector3(0, 1), gridSize * new Vector3(1, 1), Color.cyan, 30);
+        Debug.DrawLine(gridSize * new Vector3(0, 0), gridSize * new Vector3(0, 1), Color.cyan, 30);
     }
 
     //creates objects in a grid until it runs out of place attempts;
+    List<Vector3> CheckValidPosition(int x, int y)
+    {
+        List<Vector3> neighbors = new List<Vector3>();
+        List<Vector3> sides = new List<Vector3>();
 
+        var right = new Vector2(x + 1, y);
+        var left = new Vector2(x - 1, y);
+        var up = new Vector2(x,y+1);
+        var down = new Vector2(x, y - 1);
+        sides.Add(right);
+        sides.Add(up);
+        sides.Add(left);
+        sides.Add(down);
+
+/*        if (!placedRooms.Contains(right))
+            neighbors.Add(right);
+        if (!placedRooms.Contains(left))
+            neighbors.Add(left);
+        if (!placedRooms.Contains(up))
+            neighbors.Add(up);
+        if (!placedRooms.Contains(down))
+            neighbors.Add(down);*/
+
+        foreach(Vector3 pos in sides)
+        {
+            if (!placedRooms.Contains(pos))
+            {
+                if (pos.x < gridSize.x && pos.x >= 0 && pos.y < gridSize.y && pos.y >= 0)
+                {
+                    neighbors.Add(pos);
+                }
+            }
+        }
+
+        return neighbors;
+    }
 }
