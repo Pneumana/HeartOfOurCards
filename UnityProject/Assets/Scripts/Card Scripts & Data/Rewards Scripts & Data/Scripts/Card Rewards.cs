1f4887375dabdcc1b21d680e9aa102004ec82266
slugrunner874 <kingsrule30@gmail.com>
2024-03-11 17:45:05 -0700

    lmao

--- UnityProject/Assets/Scripts/Card Scripts & Data/Rewards Scripts & Data/Scripts/Card Rewards.cs
+++ UnityProject/Assets/Scripts/Card Scripts & Data/Rewards Scripts & Data/Scripts/Card Rewards.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class CardRewards : MonoBehaviour
+public class CardRewards : NetworkBehaviour
 {
     [Header("Rarity Pools")]
     [SerializeField] private CardRarieties commonCards;
@@ -33,19 +33,21 @@ public class CardRewards : MonoBehaviour
         
     }
 
-    //[Command(requiresAuthority = false)]
+    [Command(requiresAuthority = false)]
     public void CMDCardGeneration()
     {
+        if (RunManager.instance.pickingPlayer == RunManager.instance.LocalPlayerID) return;
         for (int i = 0; i < 3; i++)
         {
             var roll = Random.Range(1, 101);
-            var roll2 = Random.Range(0, commonCards.Cards.Count - 1);
-            var roll3 = Random.Range(0, uncommonCards.Cards.Count - 1);
-            var roll4 = Random.Range(0, rareCards.Cards.Count - 1);
+            var roll2 = Random.Range(0, commonCards.Cards.Count + 1);
+            var roll3 = Random.Range(0, uncommonCards.Cards.Count + 1);
+            var roll4 = Random.Range(0, rareCards.Cards.Count + 1);
             CardGeneration(roll, roll2, roll3, roll4, i);
         }
     }
 
+    [ClientRpc]
     private void CardGeneration(int roll, int roll2, int roll3, int roll4, int i)
     {
         var CardRewardListTemp = new List<CardData>();
@@ -72,11 +74,13 @@ public class CardRewards : MonoBehaviour
         }
     }
 
+    [Command(requiresAuthority = false)]
     public void CMDCardDestroy()
     {
         CardDestroy();
     }
 
+    [ClientRpc]
     private void CardDestroy()
     {
         foreach (GameObject card in CardRewardList)
@@ -84,5 +88,63 @@ public class CardRewards : MonoBehaviour
             Destroy(card);
         }
         CardRewardList.Clear();
+    }
+
+    [Command(requiresAuthority = false)]
+    public void CMDAddCard(int number)
+    {
+        AddCard(number);
+    }
+
+    //[ClientRpc]
+    //public void AddCard1()
+    //{
+    //    var card = CardRewardList[0];
+    //    string name = card.GetComponent<CardBase>().CardData.CardName;
+    //    AddCard(name);
+    //}
+
+    //[Command(requiresAuthority = false)]
+    //public void CMDAddCard2()
+    //{
+    //    AddCard2();
+    //}
+
+    //[ClientRpc]
+    //public void AddCard2()
+    //{
+    //    var card = CardRewardList[1];
+    //    string name = card.GetComponent<CardBase>().CardData.CardName;
+    //    AddCard(name);
+    //}
+
+    //[Command(requiresAuthority = false)]
+    //public void CMDAddCard3()
+    //{
+    //    AddCard3();
+    //}
+
+    //[ClientRpc]
+    //public void AddCard3()
+    //{
+    //    var card = CardRewardList[2];
+    //    string name = card.GetComponent<CardBase>().CardData.CardName;
+    //    AddCard(name);
+    //}
+
+    public void AddCard(int number)
+    {
+        var card = CardRewardList[number];
+        string name = card.GetComponent<CardBase>().CardData.CardName;
+        var load = Resources.Load<CardData>("CardData/" + name);
+
+        if (NetworkServer.connections.Count > 1)
+        {
+            AmbidexterousManager.Instance.PlayerList[RunManager.instance.pickingPlayer].decks[0].Add(load);
+        }
+        else
+        {
+            AmbidexterousManager.Instance.PlayerList[0].decks[RunManager.instance.pickingPlayer].Add(load);
+        }
     }
 }
