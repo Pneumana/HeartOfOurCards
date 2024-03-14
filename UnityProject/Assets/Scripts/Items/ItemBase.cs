92f43e0e17d687feafe8b4163dd44a31a1bf1434
Pneumana <connor_nicewarner1@my.vcccd.edu>
2024-03-14 11:21:27 -0700

    just in case push

---
+++ UnityProject/Assets/Scripts/Items/ItemBase.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{
+    //stats
+    //health
+    //stam
+    //energy
+
+    //charges (used by some items)
+
+    //proc effects [list]
+    //proc conditions
+}
---
+++ UnityProject/Assets/Scripts/Items/ItemRage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRage : ItemBase
+{
+    //increase damage when health is low for the owner.
+
+    private void Start()
+    {
+        
+    }
+    
+
+
+
+
+}
